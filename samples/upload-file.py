9beb7274e6d66cfd0412818f38ebf0b4e078a360
Rian Finnegan <rian@amphoradata.com>
2019-11-05 16:19:41 +1100

    remoce unused line

--- samples/upload-file.py
+++ samples/upload-file.py
@@ -26,7 +26,7 @@ try:
 
     dto = amphora_client.CreateAmphoraDto(name = "Create File Example", price = 0, 
         description="This is an Amphora created to show how to upload a file via the Python SDK")
-    amphoraApi.api_client
+
     a = amphoraApi.amphorae_create(dto)
     print('created amphora')
 
