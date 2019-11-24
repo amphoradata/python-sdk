6076ff741038daa7fe3f5cbf2024deb3f8ca5771
Rian Finnegan <rian@amphoradata.com>
2019-11-25 10:55:53 +1100

    0.2.3

--- generated/amphora_client/configuration.py
+++ generated/amphora_client/configuration.py
@@ -272,7 +272,7 @@ class Configuration(six.with_metaclass(TypeWithDefault, object)):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 0.2.1\n"\
-               "SDK Package Version: 0.2.2".\
+               "SDK Package Version: 0.2.3".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
