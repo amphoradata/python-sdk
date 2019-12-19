e12489136a373f27916c8f9de0919bd44c28c089
Rian Finnegan <rian@amphoradata.com>
2019-12-19 17:16:01 +1100

    version: 0.4.0

--- generated/amphora_client/configuration.py
+++ generated/amphora_client/configuration.py
@@ -272,7 +272,7 @@ class Configuration(six.with_metaclass(TypeWithDefault, object)):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 0.4.0\n"\
-               "SDK Package Version: 0.4.0.dev1".\
+               "SDK Package Version: 0.4.0".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
