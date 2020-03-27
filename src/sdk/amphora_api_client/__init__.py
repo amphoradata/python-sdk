8dcdf7d328f9481e2daf17a7e3fd2df47cd2a627
Rian Finnegan <rian@amphoradata.com>
2020-03-27 12:54:51 +1100

    dev: dev3 prerelease version

--- src/sdk/amphora_api_client/__init__.py
+++ src/sdk/amphora_api_client/__init__.py
@@ -14,7 +14,7 @@
 
 from __future__ import absolute_import
 
-__version__ = "0.9.0.dev2"
+__version__ = "0.9.0.dev3"
 
 # import apis into sdk package
 from amphora_api_client.api.amphorae_api import AmphoraeApi
