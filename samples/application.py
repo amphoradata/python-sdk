debd8a1d67f476ed6da68a9c3070b83e778294e9
Rian Finnegan <rian@amphoradata.com>
2020-06-22 17:18:49 +1000

    feat: show how to update an application

--- samples/application.py
+++ samples/application.py
@@ -1,6 +1,6 @@
 import os
 from amphora.client import AmphoraDataRepositoryClient, Credentials
-from amphora_api_client import ApplicationsApi, CreateApplication, AppLocation
+from amphora_api_client import ApplicationsApi, CreateApplication, AppLocation, UpdateApplication
 
 # provide your login credentials
 credentials = Credentials(username=os.environ['username'],
@@ -10,6 +10,7 @@ client = AmphoraDataRepositoryClient(credentials)
 
 # create an amphora
 appApi = ApplicationsApi(client.apiClient)
+
 appLocations = [
     AppLocation(origin="http://localhost:3000", allowed_redirect_paths=["/#/callback"])
 ]
@@ -22,3 +23,8 @@ app = appApi.applications_create_application(
 
 print(f'Your Application ID is {app.id}')
 
+# you can also update an application's locations
+# here we are adding the 'production' location for react-amphora-example
+appLocations.append(AppLocation(origin="https://react-amphora-example.xtellurian.vercel.app", allowed_redirect_paths=["/#/callback"]))
+updateModel = UpdateApplication(id=app.id, name=app.name, logout_url=app.logout_url, locations=appLocations)
+updatedApplication = appApi.applications_update_application(app.id, update_application=updateModel)
