832346622c2716f2dc5e442d099efd4ed43529d2
Rian Finnegan <rian@amphoradata.com>
2020-11-17 17:49:05 +1100

    download all [ci skip]

--- samples/file_paging.py
+++ samples/file_paging.py
@@ -13,17 +13,14 @@ amphora = client.get_amphora("84f36667-9bc9-4da1-baba-1ef3160eae0f")
 # skip = number of files to ignore
 # take= the max num of files in the list
 # order_by = the way to order the files. Choose Alphabetical (default) or LastModified
-files = amphora.get_files(skip=90, take=30)
+files = amphora.get_files(100, 0)
 
 print(f'The client returned a list with {len(files)} names')
 print(f'The first file name is {files[0].name}')
 
 # download a file
-f1 = files[0]
-print(f'The first file name is {f1.name}')
-f1.pull(f'./data/{f1.name}')
+for f in files:
+    print(f'Downloading {f.name}')
+    f.pull(f'./data/temp/{f.name}')
 
-# alternate way to download the file
-f2 = amphora.get_file(files[1].name)
-print(f2.name)
-f2.pull(f'./data/{f2.name}')
+print('done')
