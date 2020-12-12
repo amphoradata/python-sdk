44d20c0ee9529f934825ed01e235749151fe5fcb
Rian Finnegan <rian@amphoradata.com>
2020-12-12 11:35:21 +1100

    version 0.10.29-2

--- src/sdk/amphora_api_client/configuration.py
+++ src/sdk/amphora_api_client/configuration.py
@@ -259,7 +259,7 @@ class Configuration(object):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 0.10.29\n"\
-               "SDK Package Version: 0.10.29".\
+               "SDK Package Version: 0.10.29-2".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
